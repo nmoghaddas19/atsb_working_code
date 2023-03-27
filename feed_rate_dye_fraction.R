19883956068a16e665e1a7f639b8eae329e2eb7c
nmoghaddas19 <nimamoghaddas519@gmail.com>
2023-03-27 10:39:54 +0100

    Update feed_rate_dye_fraction.R

--- feed_rate_dye_fraction.R
+++ feed_rate_dye_fraction.R
@@ -8,8 +8,8 @@ feed_rate <- f^2/(b*u) + f/b
 plot(d, feed_rate, type="l", frame.plot = F, xlim=c(0,0.5), ylim=c(0,1))
 
 u <- seq(0,1,0.01)
-d <- 0.3
-b <- 0.6
+d <- 0.1
+b <- 0.3
 f <- u*(d+u)/(b*d+u*(d+u))
 plot(f, u, type="l", frame.plot = F, xlim=c(0,0.6), ylim=c(0,0.5), lwd=2,
      xlab="Dyed fraction", ylab="Feeding rate")
@@ -18,13 +18,13 @@ approx(f, u, xout = 0.35)
 u <- seq(0,1,0.01)
 r <- 1/4.5
 d <- 0.1
-b <- 0.3
+b <- 0.1
 f <- u*(d+u)/((b+r)*(d+r)+u*(d+u))
 plot(f, u, type="l", frame.plot = F, xlim=c(0,0.6), ylim=c(0,0.5), lwd=2,
      xlab="Dyed fraction", ylab="Feeding rate")
 
-a <- -2.058
-c <- 2.27
+a <- -1.595
+c <- 1.824
 f <- a*u^2 + c*u
 lines(f, u, type="l", xlim=c(0,0.6), ylim=c(0,0.5), lwd=2,
      xlab="Dyed fraction", ylab="Feeding rate", col=2)
